9c2f3ef57b12de8f2c923abb7822e52be90aabcd
Carlo Barone <carlo.barone@neovici.se>
2024-03-08 14:06:54 +0100

    feat(cosmoz-textarea): new contour styling for textarea

--- stories/cosmoz-textarea.stories.js
+++ stories/cosmoz-textarea.stories.js
@@ -17,5 +17,20 @@ const basic = () => html`
 			.errorMessage=${'Something is wrong!'}
 			.maxRows=${2}
 		></cosmoz-textarea>
-	`;
-export { basic, error };
+	`,
+	contour = () => html`<style>
+			cosmoz-textarea {
+				--cosmoz-input-color: #aeacac;
+				--cosmoz-input-border-radius: 4px;
+				--cosmoz-input-padding: 12px;
+				--cosmoz-input-line-display: none;
+				--cosmoz-input-contour-size: 1px;
+				--cosmoz-input-label-translate-y: 35%;
+				--cosmoz-input-input-translate-y: 8px;
+			}
+		</style>
+		<cosmoz-textarea
+			.label=${'Choose color'}
+			.value=${'Red\nGreen\nBlue'}
+		></cosmoz-textarea> `;
+export { basic, error, contour };

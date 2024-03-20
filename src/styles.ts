ab2cf6d20f98b562ae92ea908017e21599dbd604
Carlo Barone <122988480+CarloBar1@users.noreply.github.com>
2024-03-20 17:46:05 +0100

    docs(cosmoz-textarea): new sample contour styling for textarea (#78)

--- src/styles.ts
+++ src/styles.ts
@@ -38,6 +38,7 @@ export const styles = css`
 		--contour-color: var(--line-color);
 		--contour-size: var(--cosmoz-input-contour-size);
 		--label-translate-y: var(--cosmoz-input-label-translate-y, 0%);
+
 		display: block;
 		padding: var(--cosmoz-input-padding, 8px 0);
 		padding-top: var(--paper-input-container_-_padding-top, 8px);

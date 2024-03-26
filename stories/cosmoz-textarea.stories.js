4a772be04b9bfb75169a7772dfa216b75bead506
Carlo Barone <122988480+CarloBar1@users.noreply.github.com>
2024-03-26 11:15:54 +0100

    fix(inputs-float): display of float div linked to a var (#80)
    * fix(float-display): possibility to manage the display from the outside via vars
    
    * fix(styling): less whitespace between input fields
    
    * feat(float-display): float div creates too much whitespace in new design

--- stories/cosmoz-textarea.stories.js
+++ stories/cosmoz-textarea.stories.js
@@ -32,11 +32,26 @@ const basic = () => html`
 				--cosmoz-input-line-display: none;
 				--cosmoz-input-contour-size: 1px;
 				--cosmoz-input-label-translate-y: 10px;
+				--cosmoz-input-float-display: none;
+			}
+			cosmoz-textarea:not(:first-child) {
+				margin-top: 10px;
 			}
 		</style>
 		<cosmoz-textarea
 			.label=${'Write your comment here'}
 			.value=${loremIpsum}
+		></cosmoz-textarea>
+		<cosmoz-textarea
+			invalid
+			.errorMessage=${'Something is rotten in the state of Denmark'}
+			.label=${'Write another comment here'}
+			.value=${loremIpsum.toUpperCase()}
+		></cosmoz-textarea>
+		<cosmoz-textarea
+			disabled
+			.label=${'Write another comment here'}
+			.value=${'You cannot type anything here!'}
 		></cosmoz-textarea>
 	`;
 export { basic, error, contour };

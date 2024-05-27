3fb2d2e31bbe9aac63ba3d4345a8f726f829e0bf
Hassan Albaaj <hassan.albaaj@neovici.se>
2024-05-27 10:02:49 +0200

    refactor: formated code, new line in the right place

--- src/styles.ts
+++ src/styles.ts
@@ -39,8 +39,8 @@ export const styles = css`
 		--contour-color: var(--line-color);
 		--contour-size: var(--cosmoz-input-contour-size);
 		--label-translate-y: var(--cosmoz-input-label-translate-y, 0%);
-		display: block;
 		
+		display: block;
 		padding: var(--cosmoz-input-padding, 8px 0);
 		position: relative;
 		transition: transform 0.25s, width 0.25s;

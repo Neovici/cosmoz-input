12193d5066df5932810ebe243ddf51f46e475726
John <codingjohn@outlook.com>
2025-11-03 16:47:40 +0800

    feat: export render and styles from package

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from './cosmoz-input';
 export * from './cosmoz-textarea';
 export * from './cosmoz-toggle';
 export * from './use-input';
+export { render } from './render';
+export { styles } from './styles';

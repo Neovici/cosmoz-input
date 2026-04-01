1226bd9c2708d7bae98193a0505a30bf2e1fa772
eliaslandin <eliaslandin@users.noreply.github.com>
2026-04-01 13:21:30 +0000

    Deploy preview for PR 244 🛫

---
+++ pr/pr-244/assets/cosmoz-input.stories-Cts3SD5E.js
@@ -0,0 +1,214 @@
+import{b as o}from"./iframe-CyNFwirI.js";import"./cosmoz-input-BGv_5nIC.js";import{s as n}from"./style-DzbXejPt.js";import"./preload-helper-PPVm8Dsz.js";import"./use-input-DEkS1QI1.js";const{expect:d,waitFor:b}=__STORYBOOK_MODULE_TEST__,w={title:"Components/Input",component:"cosmoz-input",tags:["autodocs"]},e={render:()=>o`
+        ${n}
+        <cosmoz-input .label=${"Choose color"}></cosmoz-input>
+    `,play:async({canvas:c,step:m})=>{await m("Renders input element",async()=>{await c.findByShadowRole("textbox")})}},s={render:()=>o`
+        ${n}
+        <cosmoz-input
+            always-float-label
+            .label=${"Choose color"}
+            placeholder=${"placeholder text"}
+        ></cosmoz-input>
+    `},t={render:()=>o`
+        ${n}
+        <cosmoz-input no-label-float .label=${"Choose color"}></cosmoz-input>
+    `},a={render:()=>o`
+        ${n}
+        <cosmoz-input
+            .label=${"Choose color"}
+            .value=${"Red"}
+            invalid
+            .errorMessage=${"Something is wrong!"}
+        ></cosmoz-input>
+    `,play:async({canvasElement:c,step:m})=>{const z=c.querySelector("cosmoz-input");await m("error message is rendered",async()=>{await b(()=>{const u=z.shadowRoot.querySelector(".error");d(u).not.toBeNull(),d(u.textContent).toBe("Something is wrong!")})})}},r={render:()=>o`
+        ${n}
+        <cosmoz-input
+            autosize
+            no-label-float
+            type="number"
+            .value=${3.12}
+        ></cosmoz-input>
+    `},l={render:()=>o`
+        ${n}
+        <cosmoz-input
+            no-label-float
+            type="color"
+            .value=${"#ff0000"}
+        ></cosmoz-input>
+    `},i={render:()=>o`
+        ${n}
+        <cosmoz-input
+            type="number"
+            .label=${"This number input has no spinner"}
+            no-spinner
+        ></cosmoz-input>
+    `},p={render:()=>o`
+        ${n}
+        <style>
+            cosmoz-input {
+                --cosmoz-input-color: #aeacac;
+                --cosmoz-input-border-radius: 8px;
+                --cosmoz-input-wrap-padding: 12px 16px;
+                --cosmoz-input-padding: 14px 0px;
+                --cosmoz-input-line-display: none;
+                --cosmoz-input-contour-size: 1px;
+                --cosmoz-input-label-translate-y: 8px;
+                --cosmoz-input-float-display: none;
+            }
+            span {
+                margin: 0 4px;
+            }
+        </style>
+        <cosmoz-input .label=${"Insert a text input"}></cosmoz-input>
+        <cosmoz-input
+            .label=${"I want this input to have the suffix match the same placing!"}
+            type="number"
+            ><span slot="suffix">${"suffix"}</span></cosmoz-input
+        >
+        <cosmoz-input .label=${"Same thing of above, with the prefix!"}
+            ><span slot="prefix">${"Prefix:"}</span></cosmoz-input
+        >
+        <cosmoz-input always-float-label .label=${"This label always floats"}
+            ><span slot="prefix">${"Example:"}</span></cosmoz-input
+        >
+        <cosmoz-input
+            no-label-float
+            .label=${"This label never floats"}
+        ></cosmoz-input>
+        <cosmoz-input no-label-float .label=${"This label never floats with prefix"}
+            ><span slot="prefix">${"Example:"}</span></cosmoz-input
+        >
+        <cosmoz-input no-label-float .label=${"This label never floats with sufix"}
+            ><span slot="suffix">${"suffix"}</span></cosmoz-input
+        >
+        <cosmoz-input
+            invalid
+            .label=${"This input is invalid!"}
+            .errorMessage=${"Something is rotten in the state of Denmark."}
+        ></cosmoz-input>
+        <cosmoz-input disabled .label=${"This input is disabled!"}></cosmoz-input>
+    `};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: () => html\`
+        \${style}
+        <cosmoz-input .label=\${'Choose color'}></cosmoz-input>
+    \`,
+  play: async ({
+    canvas,
+    step
+  }) => {
+    await step('Renders input element', async () => {
+      await canvas.findByShadowRole('textbox');
+    });
+  }
+}`,...e.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  render: () => html\`
+        \${style}
+        <cosmoz-input
+            always-float-label
+            .label=\${'Choose color'}
+            placeholder=\${'placeholder text'}
+        ></cosmoz-input>
+    \`
+}`,...s.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  render: () => html\`
+        \${style}
+        <cosmoz-input no-label-float .label=\${'Choose color'}></cosmoz-input>
+    \`
+}`,...t.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  render: () => html\`
+        \${style}
+        <cosmoz-input
+            .label=\${'Choose color'}
+            .value=\${'Red'}
+            invalid
+            .errorMessage=\${'Something is wrong!'}
+        ></cosmoz-input>
+    \`,
+  play: async ({
+    canvasElement,
+    step
+  }) => {
+    const el = canvasElement.querySelector('cosmoz-input')!;
+    await step('error message is rendered', async () => {
+      await waitFor(() => {
+        const error = el.shadowRoot!.querySelector('.error');
+        expect(error).not.toBeNull();
+        expect(error!.textContent).toBe('Something is wrong!');
+      });
+    });
+  }
+}`,...a.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: () => html\`
+        \${style}
+        <cosmoz-input
+            autosize
+            no-label-float
+            type="number"
+            .value=\${3.12}
+        ></cosmoz-input>
+    \`
+}`,...r.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  render: () => html\`
+        \${style}
+        <cosmoz-input
+            no-label-float
+            type="color"
+            .value=\${'#ff0000'}
+        ></cosmoz-input>
+    \`
+}`,...l.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  render: () => html\`
+        \${style}
+        <cosmoz-input
+            type="number"
+            .label=\${'This number input has no spinner'}
+            no-spinner
+        ></cosmoz-input>
+    \`
+}`,...i.parameters?.docs?.source}}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  render: () => html\`
+        \${style}
+        <style>
+            cosmoz-input {
+                --cosmoz-input-color: #aeacac;
+                --cosmoz-input-border-radius: 8px;
+                --cosmoz-input-wrap-padding: 12px 16px;
+                --cosmoz-input-padding: 14px 0px;
+                --cosmoz-input-line-display: none;
+                --cosmoz-input-contour-size: 1px;
+                --cosmoz-input-label-translate-y: 8px;
+                --cosmoz-input-float-display: none;
+            }
+            span {
+                margin: 0 4px;
+            }
+        </style>
+        <cosmoz-input .label=\${'Insert a text input'}></cosmoz-input>
+        <cosmoz-input
+            .label=\${'I want this input to have the suffix match the same placing!'}
+            type="number"
+            ><span slot="suffix">\${'suffix'}</span></cosmoz-input
+        >
+        <cosmoz-input .label=\${'Same thing of above, with the prefix!'}
+            ><span slot="prefix">\${'Prefix:'}</span></cosmoz-input
+        >
+        <cosmoz-input always-float-label .label=\${'This label always floats'}
+            ><span slot="prefix">\${'Example:'}</span></cosmoz-input
+        >
+        <cosmoz-input
+            no-label-float
+            .label=\${'This label never floats'}
+        ></cosmoz-input>
+        <cosmoz-input no-label-float .label=\${'This label never floats with prefix'}
+            ><span slot="prefix">\${'Example:'}</span></cosmoz-input
+        >
+        <cosmoz-input no-label-float .label=\${'This label never floats with sufix'}
+            ><span slot="suffix">\${'suffix'}</span></cosmoz-input
+        >
+        <cosmoz-input
+            invalid
+            .label=\${'This input is invalid!'}
+            .errorMessage=\${'Something is rotten in the state of Denmark.'}
+        ></cosmoz-input>
+        <cosmoz-input disabled .label=\${'This input is disabled!'}></cosmoz-input>
+    \`
+}`,...p.parameters?.docs?.source}}};const g=["Basic","AlwaysFloatLabel","NoLabelFloat","Error","Autosize","Color","NoSpinner","Contour"];export{s as AlwaysFloatLabel,r as Autosize,e as Basic,l as Color,p as Contour,a as Error,t as NoLabelFloat,i as NoSpinner,g as __namedExportsOrder,w as default};

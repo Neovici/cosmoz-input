cf8c52af18da55b66d49de34d46c6173c2578038
cristinecula <cristinecula@users.noreply.github.com>
2026-09-03 10:08:51 +0000

    Remove preview for PR 376 🛬

--- pr/pr-376/assets/style-DGyL6mgw.js
+++
@@ -1,8 +0,0 @@
-import{i as e}from"./preload-helper-CCSz8wUY.js";import{c as t,s as n}from"./iframe-CJTB0Edv.js";var r,i=e((()=>{t(),r=n`
-	<style>
-		@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500&display=swap');
-		html {
-			font-family: 'Inter', sans-serif;
-		}
-	</style>
-`}));export{r as n,i as t};
